7aef6d349f7a332fb3d3ad8b2e6d597be8ded414
Alexey Morozov <moroz280294@gmail.com>
2025-12-10 14:46:57 +0300

    первый тест

--- src/test/java/CreateCourier.java
+++ src/test/java/CreateCourier.java
@@ -1,2 +1,32 @@
+import io.qameta.allure.Description;
+import io.qameta.allure.junit4.DisplayName;
+import io.restassured.http.ContentType;
+import org.apache.commons.lang3.RandomStringUtils;
+import org.junit.Before;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
+
 public class CreateCourier {
-}
+    @Test
+
+    public void get() {
+        String login = RandomStringUtils.randomAlphabetic(12);
+
+        given()
+                .baseUri( "https://qa-scooter.praktikum-services.ru/")
+                .contentType(ContentType.JSON)
+                .body("{\n" +
+                        "  \"login\": \"" + login + "\",\n" +
+                        "  \"password\": \"1234\",\n" +
+                        "  \"firstName\": \"saske\"\n" +
+                        "}")
+            .when()
+            .post("/api/v1/courier")
+            .then()
+                .statusCode(201)
+                .and()
+                .body("ok",is(true));
+}}
+
--- src/test/java/OrderList.java
+++ src/test/java/OrderList.java
@@ -1,2 +1,2 @@
-public class OrderList {
-}
+
+public class OrderList {}
