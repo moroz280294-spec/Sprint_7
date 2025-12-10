9d20036ee9e444eb84708e1378da47ed8105753e
Alexey Morozov <moroz280294@gmail.com>
2025-12-10 15:49:09 +0300

    добавил тесты

--- src/test/java/CreateCourier.java
+++ src/test/java/CreateCourier.java
@@ -1,26 +1,26 @@
-import io.qameta.allure.Description;
-import io.qameta.allure.junit4.DisplayName;
 import io.restassured.http.ContentType;
 import org.apache.commons.lang3.RandomStringUtils;
-import org.junit.Before;
 import org.junit.Test;
 
 import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.when;
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
 
 public class CreateCourier {
     @Test
 
-    public void get() {
+    public void shouldCreateCourierTest() {
         String login = RandomStringUtils.randomAlphabetic(12);
-
+        String password = RandomStringUtils.randomAlphabetic(10);
+        String firstName = RandomStringUtils.randomAlphabetic(11);
         given()
                 .baseUri( "https://qa-scooter.praktikum-services.ru/")
                 .contentType(ContentType.JSON)
                 .body("{\n" +
                         "  \"login\": \"" + login + "\",\n" +
-                        "  \"password\": \"1234\",\n" +
-                        "  \"firstName\": \"saske\"\n" +
+                        "  \"password\": \"" + password + "\",\n" +
+                        "  \"firstName\": \"" + firstName + "\"\n" +
                         "}")
             .when()
             .post("/api/v1/courier")
@@ -28,5 +28,41 @@ public class CreateCourier {
                 .statusCode(201)
                 .and()
                 .body("ok",is(true));
-}}
+}
+    @Test
+
+    public void shouldLoginCourierTest() {
+        public void shouldCreateCourierTest() {
+            String login = RandomStringUtils.randomAlphabetic(12);
+            String password = RandomStringUtils.randomAlphabetic(10);
+            String firstName = RandomStringUtils.randomAlphabetic(11);
+            given()
+                    .baseUri( "https://qa-scooter.praktikum-services.ru/")
+                    .contentType(ContentType.JSON)
+                    .body("{\n" +
+                            "  \"login\": \"" + login + "\",\n" +
+                            "  \"password\": \"" + password + "\",\n" +
+                            "  \"firstName\": \"" + firstName + "\"\n" +
+                            "}")
+                    .when()
+                    .post("/api/v1/courier")
+                    .then()
+                    .statusCode(201)
+                    .and()
+                    .body("ok",is(true));
+        }
+        given()
+                .baseUri( "https://qa-scooter.praktikum-services.ru/")
+                .contentType(ContentType.JSON)
+                .body("{\n" +
+                        "  \"login\": \"" + login + "\",\n" +
+                        "  \"password\": \"" + password + "\",\n" +
+                        "}"))
+                .when()
+                .post("/api/v1/courier/login)
+                .then()
+                .statusCode(201)
+                        .body("id",notNullValue());
+    }
+}
 
